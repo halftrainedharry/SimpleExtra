da18a744733fa6c4b3ec238153c5f29b7c7336b8
halftrainedharry <katachrese@gmx.ch>
2023-07-12 11:01:31 +0200

    Comments added to QuickAPI snippet

--- quickapi/ApiItems.snippet.php
+++ quickapi/ApiItems.snippet.php
@@ -2,6 +2,8 @@
 use SimpleExtra\Model\Item;
 
 if ($method === 'GET') {
+    // Query all the items from the database
+
     $c = $modx->newQuery(Item::class);
     $c->sortby('id', 'DESC');
     $items = $modx->getCollection(Item::class, $c);
@@ -12,26 +14,30 @@ if ($method === 'GET') {
     }
     $quickapi->setResponse(true, ['results' => $items_arr, 'total' => count($items)]);
 } elseif ($method === 'POST') {
+    // Create a new item
+
     if (!isset($body['name']) || !isset($body['description'])) {
         $quickapi->setResponse(false, [], "Missing data", 200);
         return;
     }
-    
+
     $item = $modx->newObject(Item::class);
     $item->set('name', trim($body['name']));
     $item->set('description', trim($body['description']));
-    
+
     if ($item->save()) {
         $quickapi->setResponse(true, ['object' => $item->toArray()]);
     } else {
         $quickapi->setResponse(false, [], "Item couldn't be created", 200);
     }
 } elseif ($method === 'PUT') {
+    // Change an existing item
+
     if (!isset($body['id']) || intval($body['id']) == 0) {
         $quickapi->setResponse(false, [], "id not specified!", 200);
         return;
     }
-    
+
     $item = $modx->getObject(Item::class, intval($body['id']));
     if ($item) {
         if (isset($body['name'])) {
@@ -40,7 +46,7 @@ if ($method === 'GET') {
         if (isset($body['description'])) {
             $item->set('description', trim($body['description']));
         }
-        
+
         if ($item->save()) {
             $quickapi->setResponse(true, ['object' => $item->toArray()]);
         } else {
@@ -50,11 +56,13 @@ if ($method === 'GET') {
         $quickapi->setResponse(false, [], "Item not found", 200);
     }
 } elseif ($method === 'DELETE') {
+    // Delete an item
+
     if (count($path) == 0 || intval($path[0]) == 0) {
         $quickapi->setResponse(false, [], "id not specified!", 400);
         return;
     }
-    
+
     $item = $modx->getObject(Item::class, intval($path[0]));
     if ($item) {
         if ($item->remove()) {
